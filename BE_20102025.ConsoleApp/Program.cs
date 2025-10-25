c5b0ba7028b67e07fabebaf93f6ed4d8002cd541
Trung Nguyen <nguyennttd01427@gmai.com>
2025-10-25 16:18:09 +0700

    commit lần đầu tạo base

---
+++ BE_20102025.ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BE_20102025.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
